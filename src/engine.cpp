2764bd20721b2ab0cef79bab7c019088e6b1c01d
Chris Robinson <chris.kcat@gmail.com>
2014-12-25 19:41:45 -0800

    Create an Ogre window from the SDL window

--- src/engine.cpp
+++ src/engine.cpp
@@ -5,6 +5,12 @@
 #include <iostream>
 #include <sstream>
 
+#include <OgreRoot.h>
+#include <OgreSceneManager.h>
+#include <OgreCamera.h>
+#include <OgreRenderWindow.h>
+#include <OgreViewport.h>
+
 #include <SDL.h>
 #include <SDL_syswm.h>
 
@@ -14,14 +20,45 @@ namespace TK
 
 Engine::Engine(void)
   : mSDLWindow(nullptr)
+  , mRoot(nullptr)
+  , mWindow(nullptr)
+  , mSceneMgr(nullptr)
+  , mCamera(nullptr)
+  , mViewport(nullptr)
 {
 }
 
 Engine::~Engine(void)
 {
-    // If we don't do this, the desktop resolution is not restored on exit
+    if(mRoot)
+    {
+        if(mSceneMgr)
+        {
+            if(mCamera)
+                mSceneMgr->destroyCamera(mCamera);
+            mCamera = nullptr;
+
+            mRoot->destroySceneManager(mSceneMgr);
+            mSceneMgr = nullptr;
+        }
+
+        if(mWindow)
+        {
+            if(mViewport)
+                mWindow->removeViewport(mViewport);
+            mViewport = nullptr;
+
+            mWindow->destroy();
+            mWindow = nullptr;
+        }
+
+        delete mRoot;
+        mRoot = nullptr;
+    }
+
     if(mSDLWindow)
     {
+        // If we don't do this, the desktop resolution is not restored on exit
         SDL_SetWindowFullscreen(mSDLWindow, 0);
         SDL_DestroyWindow(mSDLWindow);
         mSDLWindow = nullptr;
@@ -45,6 +82,55 @@ bool Engine::parseOptions(int argc, char *argv[])
 }
 
 
+Ogre::RenderWindow *Engine::createRenderWindow(SDL_Window *win)
+{
+    // Get the native window handle
+    struct SDL_SysWMinfo wmInfo;
+    SDL_VERSION(&wmInfo.version);
+
+    if(SDL_GetWindowWMInfo(win, &wmInfo) == SDL_FALSE)
+        throw std::runtime_error("Couldn't get WM Info!");
+
+    Ogre::NameValuePairList params;
+    Ogre::String winHandle;
+    switch(wmInfo.subsystem)
+    {
+#if defined(SDL_VIDEO_DRIVER_WINDOWS)
+    case SDL_SYSWM_WINDOWS:
+        winHandle = Ogre::StringConverter::toString((DWORD_PTR)wmInfo.info.win.window);
+        break;
+#endif
+#if defined(SDL_VIDEO_DRIVER_COCOA)
+    case SDL_SYSWM_COCOA:
+        // Required to make OGRE play nice with our window
+        params.insert(std::make_pair("macAPI", "cocoa"));
+        params.insert(std::make_pair("macAPICocoaUseNSView", "true"));
+
+        winHandle = Ogre::StringConverter::toString(WindowContentViewHandle(wmInfo));
+        break;
+#endif
+#if defined(SDL_VIDEO_DRIVER_X11)
+    case SDL_SYSWM_X11:
+        winHandle = Ogre::StringConverter::toString((unsigned long)wmInfo.info.x11.window);
+        break;
+#endif
+    default:
+        throw std::runtime_error("Unsupported windowing subsystem: " +
+                                 Ogre::StringConverter::toString(wmInfo.subsystem));
+    }
+    /* TODO: externalWindowHandle is deprecated according to the source code.
+     * Figure out a way to get parentWindowHandle to work properly. On Linux/
+     * X11 it causes an occasional GLXBadDrawable error. */
+    params.insert(std::make_pair("externalWindowHandle", std::move(winHandle)));
+
+    int width, height;
+    Ogre::String title = SDL_GetWindowTitle(win);
+    SDL_GetWindowSize(win, &width, &height);
+    bool fs = (SDL_GetWindowFlags(win)&SDL_WINDOW_FULLSCREEN);
+    return mRoot->createRenderWindow(title, width, height, fs, &params);
+}
+
+
 bool Engine::go(void)
 {
     // Kindly ask SDL not to trash our OGL context
@@ -57,10 +143,17 @@ bool Engine::go(void)
         return false;
     }
 
+    // construct Ogre::Root
+    mRoot = new Ogre::Root("plugins.cfg");
+
     // configure
     {
-        int width = 1024;
-        int height = 768;
+        // Show the configuration dialog and initialise the system
+        if(!(mRoot->restoreConfig() || mRoot->showConfigDialog()))
+            return false;
+
+        int width = 1280;
+        int height = 720;
         int xpos = SDL_WINDOWPOS_CENTERED;
         int ypos = SDL_WINDOWPOS_CENTERED;
         Uint32 flags = SDL_WINDOW_SHOWN;
@@ -71,6 +164,9 @@ bool Engine::go(void)
             std::cerr<< "SDL_CreateWindow Error: "<<SDL_GetError() <<std::endl;
             return false;
         }
+
+        mRoot->initialise(false);
+        mWindow = createRenderWindow(mSDLWindow);
     }
 
     SDL_Event evt;
--- src/engine.hpp
+++ src/engine.hpp
@@ -3,12 +3,29 @@
 
 struct SDL_Window;
 
+namespace Ogre
+{
+    class Root;
+    class RenderWindow;
+    class SceneManager;
+    class Camera;
+    class Viewport;
+}
+
 namespace TK
 {
 
 class Engine
 {
     SDL_Window *mSDLWindow;
+
+    Ogre::Root *mRoot;
+    Ogre::RenderWindow *mWindow;
+    Ogre::SceneManager *mSceneMgr;
+    Ogre::Camera *mCamera;
+    Ogre::Viewport *mViewport;
+
+    Ogre::RenderWindow *createRenderWindow(SDL_Window *win);
 
 public:
     Engine(void);
