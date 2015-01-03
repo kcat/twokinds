df1bf76f6f97d86f1fd52d2928a5a58fec33cd14
Chris Robinson <chris.kcat@gmail.com>
2015-01-03 06:35:08 -0800

    Add bilinear interpolation to the image source module

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -149,21 +149,39 @@ public:
 
     virtual double GetValue(double x, double /*y*/, double z) const
     {
+        size_t width = mImage.getWidth();
+        size_t height = mImage.getHeight();
+
         // NOTE: X is west/east and Z is north/south (Y is up/down, but we
         // don't bother with depth)
         x *= mFrequency;
         z *= mFrequency;
 
         // Offset the image so 0 is the center
-        x += mImage.getWidth()/2.0;
-        z += mImage.getHeight()/2.0;
+        x += width/2.0;
+        z += height/2.0;
 
-        x = Ogre::Math::Clamp<double>(x, 0.0, mImage.getWidth()-1);
-        z = Ogre::Math::Clamp<double>(z, 0.0, mImage.getHeight()-1);
+        x = size_t(Ogre::Math::Clamp<double>(x, 0.0, width-1));
+        z = size_t(Ogre::Math::Clamp<double>(z, 0.0, height-1));
 
-        Ogre::ColourValue clr = mImage.getColourAt((size_t)x, (size_t)z, 0);
+        size_t sx = size_t(x);
+        size_t sy = size_t(z);
+
+        x = x - sx;
+        z = z - sy;
+
+        float b00 = (1.0-x) * (1.0-z);
+        float b01 = (    x) * (1.0-z);
+        float b10 = (1.0-x) * (    z);
+        float b11 = (    x) * (    z);
+
+        // Bilinear interpolate using the four nearest colors
+        Ogre::ColourValue clr00 = mImage.getColourAt(sx, sy, 0);
+        Ogre::ColourValue clr01 = mImage.getColourAt(std::min(sx+1, width-1), sy, 0);
+        Ogre::ColourValue clr10 = mImage.getColourAt(sx, std::min(sy+1,height-1), 0);
+        Ogre::ColourValue clr11 = mImage.getColourAt(std::min(sx+1, width-1), std::min(sy+1,height-1), 0);
         // The components are normalized to 0...1, while libnoise expects -1...+1.
-        return clr.r*2.0 - 1.0;
+        return (clr00.r*b00 + clr01.r*b01 + clr10.r*b10 + clr11.r*b11)*2.0 - 1.0;
     }
 };
 
